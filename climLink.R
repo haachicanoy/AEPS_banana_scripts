dd8e99bb238fc7b26f3619d4e6919b6d0e6b40e6
haachicanoy <harold22010@gmail.com>
2015-11-26 09:17:03 -0500

    Consolidated analysis

--- climLink.R
+++ climLink.R
@@ -37,10 +37,12 @@ baseClima$FECHA <- as.Date(baseClima$FECHA, format='%Y-%m-%d')
 
 # Fix date format
 
-baseManejo$fechaCosecha <- as.Date(baseManejo$fechaCosecha, format='%Y-%m-%d')
-baseManejo$fechaSiembra <- as.Date(baseManejo$fechaCosecha-321)
+x <- seq(from=as.Date("2000-01-01", format='%Y-%m-%d'), to=as.Date("2015-12-31", format='%Y-%m-%d'), by='day')
+x <- x[match(paste(baseManejo$Year_cosecha, '-', baseManejo$Semana_cosecha, sep=''), format(x, "%Y-%U"))]
 
-baseManejo <- baseManejo[which(!as.vector(is.na(baseManejo$fechaCosecha))),]
+baseManejo$fechaCosecha <- x; rm(x)
+baseManejo <- baseManejo[which(!is.na(baseManejo$fechaCosecha)),]
+baseManejo$fechaSiembra <- as.Date(baseManejo$fechaCosecha-267)
 rownames(baseManejo) <- 1:nrow(baseManejo)
 
 #CREAR INDICADORES CLIMATICOS POR FASE DEL CULTIVO
@@ -53,89 +55,14 @@ climVar <- c("mean(TMAX)","mean(TMIN)","mean((TMAX+TMIN)/2)","mean(TMAX-TMIN)","
 namFun  <- c("TX_avg","TM_avg","T_avg","Diurnal_Range_avg",
              "TX_freq_35","P_accu","P_10_freq","RH_avg", "SR_accu")
 
-periodBase     <- 120 # Duracion total ciclo productivo                               Default values: periodBase  <- 120
-FaseCultivo    <- c("ALL") # Nombre corto por etapas de cultivo                       Default values: FaseCultivo <- c("VEG","REP","LLEN")
-diasPorFase    <- c(120) # Dias de cada etapa                                         Default values: diasPorFase <- <- c(48,41,31)
-namFec         <- c("fechaSiembra","fechaCosecha") # Nombres de la fecha de siembra   Default values: namFec      <- c("fecha_siembra","fecha_cosecha")
-
-# Calculo de indicadores climaticos
-
-calculoIndicador <- function(climDB,stageIni,stageEnd,climVar)
-{
-  nEvents <- 1:length(stageIni)
-  clIndFin <- data.frame({
-    do.call(rbind,{
-      lapply(nEvents,
-             function(x){
-               climEvent <- climDB[climDB[,1] %in% as.Date(stageIni[x]:stageEnd[x], 
-                                                           origin="1970-01-01"),]
-               return(with(climEvent,{climInd <- unlist(lapply(climVar,
-                                                               function(y){
-                                                                 eval(parse(text=y))
-                                                               }
-               ));climInd})
-               )
-             } 
-      )}
-    )
-  })
-  names(clIndFin) <- namFun
-  clIndFin
-}
-
-climIndicatorsGenerator <- function(climVar,namFun,Fase,periodcul,diasFase,cosechBase,namFecha,climBase)
-{
-  
-  rnames <- row.names(cosechBase)
-  percDias <- diasFase/periodcul
-  diasCult <- as.numeric(cosechBase[,namFecha[2]]-cosechBase[,namFecha[1]])
-  
-  diaAcum <- round(do.call(rbind,lapply(diasCult,function(x){x*cumsum(percDias)})),0)#[,-length(diasFase)]
-  
-  cropDates <- do.call(data.frame,
-                       lapply(1:ncol(diaAcum),function(x){
-                         as.Date(cosechBase[,namFecha[1]]+diaAcum[,x], origin="1899-12-30")
-                       }
-                       )
-  )
-  
-  names(cropDates) <- paste("Stage",1:ncol(cropDates),sep="_")
-  
-  cosechBase <- data.frame(cosechBase,cropDates) 
-  
-  stageDatesNam <- c(namFecha[1],names(cropDates),namFecha[2])
-  
-  stageDates <- with(cosechBase,cosechBase[stageDatesNam])
-  
-  totStg <- length(Fase)
-  
-  stageInd <- 1:totStg
-  data.frame(cropDates,
-             do.call(data.frame,
-                     lapply(stageInd,function(i){
-                       if(i==totStg)
-                       {
-                         stage1 <- stageDates[,i]
-                         stage2 <- stageDates[,i+1]   
-                         cInd <- calculoIndicador(climBase,stage1,stage2,climVar)
-                         names(cInd) <- paste(names(cInd),Fase[i],sep="_")
-                         
-                       }else{
-                         stage1 <- stageDates[,i]
-                         stage2 <- stageDates[,i+1]  -1                    
-                         cInd <- calculoIndicador(climBase,stage1,stage2,climVar)
-                         names(cInd) <- paste(names(cInd),Fase[i],sep="_")
-                       }
-                       return(cInd)
-                     }
-                     )
-             )
-  )
-}
+periodBase     <- 267                              # Duracion total ciclo productivo      Default values: periodBase  <- 120
+FaseCultivo    <- c("LEAF","DIFF","FLOW","DEVL")   # Nombre corto por etapas de cultivo   Default values: FaseCultivo <- c("VEG","REP","LLEN")
+diasPorFase    <- c(105, 49, 35, 78)               # Dias de cada etapa                   Default values: diasPorFase <- <- c(48,41,31)
+namFec         <- c("fechaSiembra","fechaCosecha") # Nombres de la fecha de siembra       Default values: namFec <- c("fecha_siembra","fecha_cosecha")
 
 a <- climIndicatorsGenerator(climVar=climVar, namFun=namFun, Fase=FaseCultivo,
                              periodcul=periodBase, diasFase=diasPorFase, cosechBase=baseManejo,
                              namFecha=namFec, climBase=baseClima)
 
 # ESCRIBIR LOS DATOS
-write.csv(data.frame(baseManejo,a),"indicadoresClimaticos.csv")
+write.csv(data.frame(baseManejo,a),"indicadoresClimaticos.csv", row.names=FALSE)
---
+++ regression_methods.R
@@ -0,0 +1,282 @@
+# file data-analysis-AEPS-BigData.R
+# 
+# This file contains a script to develop regressions with machine learning methodologies
+#
+#
+# author: Hugo Andres Dorado 02-16-2015
+#  
+#This script is free: you can redistribute it and/or modify
+#
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+
+#-----------------------------------------------------------------------------------------------------------------
+
+
+#SCRIPT BUILED FOR R VERSION 3.0.2 
+#PACKAGES
+options(warn=-1)
+if(!require(gtools)){install.packages('gtools');library(gtools)} else{library(gtools)}
+if(!require(gridBase)){install.packages('gridBase');library(gridBase)} else{library(gridBase)}
+if(!require(relaimpo)){install.packages('relaimpo');library(relaimpo)} else{library(relaimpo)}
+if(!require(caret)){install.packages('caret');library(caret)} else{library(caret)}
+if(!require(party)){install.packages('party');library(party)} else{library(party)}
+if(!require(randomForest)){install.packages('randomForest');library(randomForest)} else{library(randomForest)}
+if(!require(snowfall)){install.packages('snowfall');library(snowfall)} else{library(snowfall)}
+if(!require(earth)){install.packages('earth');library(earth)} else{library(earth)}
+if(!require(reshape)){install.packages('reshape');library(reshape)} else{library(reshape)}
+if(!require(agricolae)){install.packages('agricolae');library(agricolae)} else{library(agricolae)}
+if(!require(stringr)){install.packages('stringr');library(stringr)} else{library(stringr)}
+
+#Work Directory
+
+dirFol  <- "/mnt/workspace_cluster_6/TRANSVERSAL_PROJECTS/MADR/COMPONENTE_2/ASBAMA/DATOS_PROCESADOS/_clima/IDEAM/Climate_to"
+# dirFol  <- "//dapadfs/workspace_cluster_6/TRANSVERSAL_PROJECTS/MADR/COMPONENTE_2/ASBAMA/DATOS_PROCESADOS/_clima/IDEAM/Climate_to"
+setwd(dirFol)
+
+#DataBase structure
+
+dataSet <- read.csv('climateINDTest.csv')
+dataSet$splitVar <- 'All'
+
+inputs  <- 1:73  # inputs columns
+segme   <- 75    # split column
+output  <- 74    # output column
+
+namsDataSet <- names(dataSet)
+
+#Creating the split factors
+
+contVariety <- table(dataSet[,segme])
+variety0    <- names(sort(contVariety[contVariety>=30]))
+
+if(length(variety0)==1){variety = variety0 }else{variety = factor(c(variety0,"All"))}
+
+load('All-Functions-AEPS_BD.RData')
+
+variety <- 'All'
+
+#creating folders
+createFolders(dirFol, variety)
+
+#DataSets ProcesosF
+dataSetProces(variety, dataSet, segme, corRed="caret")
+
+# LINEAR REGRESSION; only when all inputs are cuantitative;  
+# lineaRegresionFun(variety, dirLocation=paste0(getwd(),"/"), ylabs="Yield (Ton/Ha)")
+
+# MULTILAYER PERCEPTRON
+multilayerPerceptronFun(variety, dirLocation=paste0(getwd(),"/"), nb.it=30, ylabs="Yield (Ton/Ha)", pertuRelevance=T, ncores=3) # All
+
+# RANDOM FOREST
+randomForestFun(variety, nb.it=30, ncores=23, wid=800, hei=1300) # All
+
+# CONDITIONAL FOREST; especify if you have categorical variables
+conditionalForestFun(variety, nb.it=30, ncores=23) # All
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------- #
+# Using only climate
+# ----------------------------------------------------------------------------------------------------------------------------------------------------- #
+
+#SCRIPT BUILED FOR R VERSION 3.0.2 
+#PACKAGES
+options(warn=-1)
+if(!require(gtools)){install.packages('gtools');library(gtools)} else{library(gtools)}
+if(!require(gridBase)){install.packages('gridBase');library(gridBase)} else{library(gridBase)}
+if(!require(relaimpo)){install.packages('relaimpo');library(relaimpo)} else{library(relaimpo)}
+if(!require(caret)){install.packages('caret');library(caret)} else{library(caret)}
+if(!require(party)){install.packages('party');library(party)} else{library(party)}
+if(!require(randomForest)){install.packages('randomForest');library(randomForest)} else{library(randomForest)}
+if(!require(snowfall)){install.packages('snowfall');library(snowfall)} else{library(snowfall)}
+if(!require(earth)){install.packages('earth');library(earth)} else{library(earth)}
+
+#Work Directory
+
+dirFol  <- "D:/ToBackup/AEPS-Big_data/Flagship_arroz_Peru/arroz_jaen/Analisis/_climate_ts/"
+# dirFol  <- "/home/hachicanoy/jaen_analysis/"
+setwd(dirFol)
+
+data2 <- read.csv('./only_climate.csv', row.names=1)
+
+#DataBase structure
+
+data2$splitVar <- 'All'
+data2 <- data2[,c('variedad',"TX_avg_ALL","TM_avg_ALL","T_avg_ALL","Diurnal_Range_avg_ALL","TX_freq_35_ALL","P_accu_ALL","P_10_freq_ALL","RH_avg_ALL",'splitVar','RDT')]
+data2$tipoSiembra <- as.factor(data2$tipoSiembra)
+data2$variedad <- as.factor(data2$variedad)
+data2$splitVar <- as.factor(data2$splitVar)
+
+inputs  <- 1:10  # inputs columns
+segme   <- 10    # split column
+output  <- 11    # output column
+
+dataSet   <- data2
+namsDataSet <- names(dataSet)
+
+#Creating the split factors
+
+contVariety <- table(dataSet[,segme])
+variety0    <- names(sort(contVariety[contVariety>=30]))
+
+if(length(variety0)==1){variety = variety0 }else{variety = factor(c(variety0,"All"))}
+
+#creating folders
+
+# load('/home/hachicanoy/jaen_analysis/All-Functions-AEPS_BD.RData')
+load("/mnt/workspace_cluster_6/TRANSVERSAL_PROJECTS/MADR/COMPONENTE_2/OPEN_BIGDATA_AEPS/REGRESSION_MODELS/All-Functions-AEPS_BD.RData")
+variety <- 'All'
+
+createFolders(dirFol, variety)
+
+#DataSets ProcesosF
+dataSetProces(variety=variety, dataSet=dataSet, segme=segme, corRed="caret")
+
+# LINEAR REGRESSION; only when all inputs are cuantitative;  
+# lineaRegresionFun(variety, dirLocation=paste0(getwd(),"/"), ylabs="Yield (Ton/Ha)")
+
+# MULTILAYER PERCEPTRON
+multilayerPerceptronFun(variety, dirLocation=paste0(getwd(),"/"), nb.it=30, ylabs="Yield (Ton/Ha)", pertuRelevance=T, ncores=3) # All
+
+# RANDOM FOREST
+randomForestFun(variety, nb.it=30, ncores=23) # All
+
+# CONDITIONAL FOREST; especify if you have categorical variables
+conditionalForestFun(variety, nb.it=30, ncores=23) # All
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------- #
+# Using both climate and soils
+# ----------------------------------------------------------------------------------------------------------------------------------------------------- #
+
+#SCRIPT BUILED FOR R VERSION 3.0.2 
+#PACKAGES
+options(warn=-1)
+if(!require(gtools)){install.packages('gtools');library(gtools)} else{library(gtools)}
+if(!require(gridBase)){install.packages('gridBase');library(gridBase)} else{library(gridBase)}
+if(!require(relaimpo)){install.packages('relaimpo');library(relaimpo)} else{library(relaimpo)}
+if(!require(caret)){install.packages('caret');library(caret)} else{library(caret)}
+if(!require(party)){install.packages('party');library(party)} else{library(party)}
+if(!require(randomForest)){install.packages('randomForest');library(randomForest)} else{library(randomForest)}
+if(!require(snowfall)){install.packages('snowfall');library(snowfall)} else{library(snowfall)}
+if(!require(earth)){install.packages('earth');library(earth)} else{library(earth)}
+
+#Work Directory
+
+dirFol  <- "D:/ToBackup/AEPS-Big_data/Flagship_arroz_Peru/arroz_jaen/Analisis/_climate_ts/"
+# dirFol  <- "/home/hachicanoy/jaen_analysis/"
+setwd(dirFol)
+
+data2 <- read.csv('./all_soil_climate.csv', row.names=1)
+
+#DataBase structure
+
+data2$splitVar <- 'All'
+data2 <- data2[,c('variedad',"CE","pH","arena","arcilla","limo","matOrganica","P_disponible","K_disponible","CIC","CaCO3","TX_avg_ALL","TM_avg_ALL","T_avg_ALL","Diurnal_Range_avg_ALL","TX_freq_35_ALL","P_accu_ALL","P_10_freq_ALL","RH_avg_ALL",'splitVar','RDT')]
+data2$variedad <- as.factor(data2$variedad)
+data2$splitVar <- as.factor(data2$splitVar)
+
+inputs  <- 1:19  # inputs columns
+segme   <- 20    # split column
+output  <- 21    # output column
+
+dataSet   <- data2
+namsDataSet <- names(dataSet)
+
+#Creating the split factors
+
+contVariety <- table(dataSet[,segme])
+variety0    <- names(sort(contVariety[contVariety>=30]))
+
+if(length(variety0)==1){variety = variety0 }else{variety = factor(c(variety0,"All"))}
+
+#creating folders
+
+# load('/home/hachicanoy/jaen_analysis/All-Functions-AEPS_BD.RData')
+load("/mnt/workspace_cluster_6/TRANSVERSAL_PROJECTS/MADR/COMPONENTE_2/OPEN_BIGDATA_AEPS/REGRESSION_MODELS/All-Functions-AEPS_BD.RData")
+variety <- 'All'
+
+createFolders(dirFol, variety)
+
+#DataSets ProcesosF
+dataSetProces(variety=variety, dataSet=dataSet, segme=segme, corRed="caret")
+
+# LINEAR REGRESSION; only when all inputs are cuantitative;  
+# lineaRegresionFun(variety, dirLocation=paste0(getwd(),"/"), ylabs="Yield (Ton/Ha)")
+
+# MULTILAYER PERCEPTRON
+multilayerPerceptronFun(variety, dirLocation=paste0(getwd(),"/"), nb.it=30, ylabs="Yield (Ton/Ha)", pertuRelevance=T, ncores=3) # All
+
+# RANDOM FOREST
+randomForestFun(variety, nb.it=30, ncores=3) # All
+
+# CONDITIONAL FOREST; especify if you have categorical variables
+conditionalForestFun(variety, nb.it=30, ncores=23) # All
+
+# ----------------------------------------------------------------------------------------------------------------------------------------------------- #
+# Using only soils
+# ----------------------------------------------------------------------------------------------------------------------------------------------------- #
+
+#SCRIPT BUILED FOR R VERSION 3.0.2 
+#PACKAGES
+options(warn=-1)
+if(!require(gtools)){install.packages('gtools');library(gtools)} else{library(gtools)}
+if(!require(gridBase)){install.packages('gridBase');library(gridBase)} else{library(gridBase)}
+if(!require(relaimpo)){install.packages('relaimpo');library(relaimpo)} else{library(relaimpo)}
+if(!require(caret)){install.packages('caret');library(caret)} else{library(caret)}
+if(!require(party)){install.packages('party');library(party)} else{library(party)}
+if(!require(randomForest)){install.packages('randomForest');library(randomForest)} else{library(randomForest)}
+if(!require(snowfall)){install.packages('snowfall');library(snowfall)} else{library(snowfall)}
+if(!require(earth)){install.packages('earth');library(earth)} else{library(earth)}
+
+#Work Directory
+
+dirFol  <- "D:/ToBackup/AEPS-Big_data/Flagship_arroz_Peru/arroz_jaen/Analisis/_climate_ts/"
+# dirFol  <- "/home/hachicanoy/jaen_analysis/"
+setwd(dirFol)
+
+data2 <- read.csv('./only_soil.csv')
+
+#DataBase structure
+
+data2$splitVar <- 'All'
+data2 <- data2[,c('tipoSiembra','variedad',"CE","pH","arena","arcilla","limo","matOrganica","P_disponible","K_disponible","CIC","CaCO3",'splitVar','RDT')]
+data2$tipoSiembra <- as.factor(data2$tipoSiembra)
+data2$variedad <- as.factor(data2$variedad)
+data2$splitVar <- as.factor(data2$splitVar)
+
+inputs  <- 1:12  # inputs columns
+segme   <- 13    # split column
+output  <- 14    # output column
+
+dataSet   <- data2
+namsDataSet <- names(dataSet)
+
+#Creating the split factors
+
+contVariety <- table(dataSet[,segme])
+variety0    <- names(sort(contVariety[contVariety>=30]))
+
+if(length(variety0)==1){variety = variety0 }else{variety = factor(c(variety0,"All"))}
+
+#creating folders
+
+# load('/home/hachicanoy/jaen_analysis/All-Functions-AEPS_BD.RData')
+load("/mnt/workspace_cluster_6/TRANSVERSAL_PROJECTS/MADR/COMPONENTE_2/OPEN_BIGDATA_AEPS/REGRESSION_MODELS/All-Functions-AEPS_BD.RData")
+variety <- 'All'
+
+createFolders(dirFol, variety)
+
+#DataSets ProcesosF
+dataSetProces(variety=variety, dataSet=dataSet, segme=segme, corRed="caret")
+
+# LINEAR REGRESSION; only when all inputs are cuantitative;  
+# lineaRegresionFun(variety, dirLocation=paste0(getwd(),"/"), ylabs="Yield (Ton/Ha)")
+
+# MULTILAYER PERCEPTRON
+multilayerPerceptronFun(variety, dirLocation=paste0(getwd(),"/"), nb.it=30, ylabs="Yield (Ton/Ha)", pertuRelevance=T, ncores=3) # All
+
+# RANDOM FOREST
+randomForestFun(variety, nb.it=30, ncores=3) # All
+
+# CONDITIONAL FOREST; especify if you have categorical variables
+conditionalForestFun(variety, nb.it=30, ncores=23) # All
